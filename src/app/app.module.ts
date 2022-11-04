caba2282a647323563fceaa5aa3185aab3370a72
abrahao-gabriel <57734187+abrahao-gabriel@users.noreply.github.com>
2022-11-04 20:05:03 -0300

    7 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,22 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TimerComponent } from './components/timer/timer.component';
-
+import { TaskService } from './services/task.service';
+import { TimerService } from './services/timer.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HomeComponent, TasksComponent, TimerComponent],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'tasks', component: TasksComponent },
+      { path: 'timer', component: TimerComponent },
+    ]),
+  ],
+  declarations: [AppComponent, HomeComponent, TimerComponent, TasksComponent],
+  bootstrap: [AppComponent],
+  providers: [TaskService, TimerService],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/timer.service.ts
+++ src/app/services/timer.service.ts
@@ -3,6 +3,19 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class TimerService {
 
-  constructor() { }
+  private timer: any;
+  private counter:number = 0;
+
+
+  constructor() { 
+    this.timer = setInterval(() => {
+      this.counter++;
+    }, 1000);
+
+  }
+
+  getCounter() {
+    return this.counter;
+  }
 
 }
